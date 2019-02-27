e51e449708bc4a0fb881aef10c5de6f9b3e6867d
satish kumar <satish.kumar2@evry.com>
2019-02-27 10:46:53 +0530

    fix Access-Control-Allow-Origin

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 import pandas as pd
 import json
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/drugabacus/getalldisease',methods=['GET'])
 def getAllDisease():
